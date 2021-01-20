68fc0798b2217ce6b9d77da6a8a53eeeb7b601cb
Tscream <Tiyani@live.nl>
2021-01-20 16:03:30 +0100

    FirstPersonMovement

--- Assets/Scripts/FPSCam.cs
+++ Assets/Scripts/FPSCam.cs
@@ -4,25 +4,67 @@ using UnityEngine;
 
 public class FPSCam : MonoBehaviour
 {
-    public float mouseSpeed = 100f;
+    public float mouseSensitivity;
+    public float moveSpeed;
+    public float jumpForce;
     public Transform head;
-    float xRotation = 0f;
+    public LayerMask mask;
+
+    Rigidbody rb;
+    float xRotation;
+    bool onGround;
 
     private void Awake()
     {
         Cursor.lockState = CursorLockMode.Locked;
     }
 
-    void Update()
+    private void Start()
     {
-        float mouseX = Input.GetAxis("Mouse X") * mouseSpeed * Time.deltaTime;
-        float mouseY = Input.GetAxis("Mouse Y") * mouseSpeed * Time.deltaTime;
+        rb = GetComponent<Rigidbody>();
+    }
+
+    private void FixedUpdate()
+    {
+        float mouseY = Input.GetAxis("Mouse Y") * mouseSensitivity;
 
         xRotation -= mouseY;
         xRotation = Mathf.Clamp(xRotation, -90f, 65f);
-
         head.transform.localRotation = Quaternion.Euler(xRotation, 0f, 0f);
-        transform.Rotate(Vector3.up * mouseX);
+        rb.MoveRotation(rb.rotation * Quaternion.Euler(new Vector3(0, Input.GetAxis("Mouse X") * mouseSensitivity, 0)));
+        rb.MovePosition(transform.position + (transform.forward * Input.GetAxis("Vertical") * moveSpeed / 100) + (transform.right * Input.GetAxis("Horizontal") * moveSpeed / 100));
+        if (Input.GetKeyDown("space") && onGround)
+        {
+            rb.AddForce(transform.up * jumpForce * 10, ForceMode.Impulse);
+        }
+        if (Input.GetButtonDown("Fire1"))
+        {
+            Debug.DrawRay(head.transform.position, head.transform.forward * 100, Color.red, 0.1f);
+            Shoot();
+        }
+    }
 
+    void Shoot()
+    {
+        RaycastHit hit;
+        if (Physics.Raycast(head.transform.position, head.transform.forward, out hit, 100, mask))
+        {
+            print(hit.transform.gameObject);
+        }
+    }
+
+    private void OnCollisionEnter(Collision collision)
+    {
+        if (collision.gameObject.tag == "Ground")
+        {
+            onGround = true;
+        }
+    }
+    private void OnCollisionExit(Collision collision)
+    {
+        if (collision.gameObject.tag == "Ground")
+        {
+            onGround = false;
+        }
     }
 }
