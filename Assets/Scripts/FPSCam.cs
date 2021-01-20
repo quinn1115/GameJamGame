a18a294f60b9308b2f0e834c06bb4c53169a01ab
Tscream <Tiyani@live.nl>
2021-01-20 14:53:53 +0100

    CamRotation

---
+++ Assets/Scripts/FPSCam.cs
@@ -0,0 +1,28 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class FPSCam : MonoBehaviour
+{
+    public float mouseSpeed = 100f;
+    public Transform head;
+    float xRotation = 0f;
+
+    private void Awake()
+    {
+        Cursor.lockState = CursorLockMode.Locked;
+    }
+
+    void Update()
+    {
+        float mouseX = Input.GetAxis("Mouse X") * mouseSpeed * Time.deltaTime;
+        float mouseY = Input.GetAxis("Mouse Y") * mouseSpeed * Time.deltaTime;
+
+        xRotation -= mouseY;
+        xRotation = Mathf.Clamp(xRotation, -90f, 65f);
+
+        head.transform.localRotation = Quaternion.Euler(xRotation, 0f, 0f);
+        transform.Rotate(Vector3.up * mouseX);
+
+    }
+}
