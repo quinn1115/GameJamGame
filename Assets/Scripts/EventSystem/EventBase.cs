af02e8765661d266f6fd87dd051f21b35d36859b
Quinn McGregor <quinn.mcgregor@hotmail.com>
2021-01-21 14:58:44 +0100

    Added EventManager & Events (W.I.P), Tested PostProcessing with Camera Layers

---
+++ Assets/Scripts/EventSystem/EventBase.cs
@@ -0,0 +1,33 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class EventBase : MonoBehaviour
+{
+
+	private bool m_EventStarted;
+
+	// Update is called once per frame
+	void Update()
+	{
+		if (m_EventStarted)
+		{
+			EventUpdate();
+		}
+	}		
+	
+	public virtual void EventStart()
+	{
+		Debug.Log("Event Started");
+		m_EventStarted = true;
+	}
+
+	public virtual void EventStop()
+	{
+		Debug.Log("Event Stopped");
+		m_EventStarted = false;
+	}
+
+	public virtual void EventUpdate()
+	{}
+}
---
+++ Assets/Scripts/EventSystem/FireEvent.cs
@@ -0,0 +1,21 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class FireEvent : EventBase
+{
+	public override void EventStart()
+	{
+		Debug.Log("Fire will start spreading soon!");
+	}
+
+	public override void EventStop()
+	{
+		Debug.Log("Fire has stopped spreading!");
+	}
+
+	public override void EventUpdate()
+	{
+		base.EventUpdate();
+	}
+}
