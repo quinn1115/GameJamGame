641d1ef7005c20a5691c8faa0492384591518684
Gordon home PC <aardappel156@gmail.com>
2021-01-25 16:11:45 +0100

    Added transition

--- Assets/Scripts/Button/ButtonChangeScene.cs
+++ Assets/Scripts/Button/ButtonChangeScene.cs
@@ -6,8 +6,19 @@ using UnityEngine;
 public class ButtonChangeScene : ButtonCollider
 {
     [SerializeField] private string sceneName;
+    [SerializeField] private Animator anim;
 
+
+    private void Start()
+    {
+        anim = GetComponent<Animator>();
+    }
     protected override void OnClick()
+    {
+        anim.SetTrigger("TransitionOn");
+    }
+
+    public void LoadScene()
     {
         SceneManager.LoadSceneAsync(sceneName);
     }
