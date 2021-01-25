f25d83c89fe8b161d9f49d5e990f0698260c559b
Quinn McGregor <quinn.mcgregor@hotmail.com>
2021-01-25 21:04:45 +0100

    Updated Player Ground Check & Prefab

--- Assets/Scripts/FPSCam.cs
+++ Assets/Scripts/FPSCam.cs
@@ -4,53 +4,46 @@ using UnityEngine;
 
 public class FPSCam : MonoBehaviour
 {
-    public float mouseSensitivity;
-    public float moveSpeed;
-    public float jumpForce;
-    public Transform head;
-    public LayerMask mask;
+	public float mouseSensitivity;
+	public float moveSpeed;
+	public float jumpForce;
+	public Transform head;
+	public LayerMask mask;
 
-    Rigidbody rb;
-    float xRotation;
-    bool onGround;
+	Rigidbody rb;
+	float xRotation;
 
-    private void Awake()
-    {
-        Cursor.lockState = CursorLockMode.Locked;
-    }
+	private void Awake()
+	{
+		Cursor.lockState = CursorLockMode.Locked;
+	}
 
-    private void Start()
-    {
-        rb = GetComponent<Rigidbody>();
-    }
+	private void Start()
+	{
+		rb = GetComponent<Rigidbody>();
+	}
 
-    private void FixedUpdate()
-    {
-        float mouseY = Input.GetAxis("Mouse Y") * mouseSensitivity;
+	private void Update()
+	{
+		float mouseY = Input.GetAxis("Mouse Y") * mouseSensitivity;
+		xRotation -= mouseY;
+		xRotation = Mathf.Clamp(xRotation, -90f, 65f);
+		head.transform.localRotation = Quaternion.Euler(xRotation, 0f, 0f);
+		if (Input.GetKeyDown("space") && CheckGrounded())
+		{
+			Debug.Log(CheckGrounded());
+			rb.AddForce(transform.up * jumpForce * 10, ForceMode.Impulse);
+		}
+	}
 
-        xRotation -= mouseY;
-        xRotation = Mathf.Clamp(xRotation, -90f, 65f);
-        head.transform.localRotation = Quaternion.Euler(xRotation, 0f, 0f);
-        rb.MoveRotation(rb.rotation * Quaternion.Euler(new Vector3(0, Input.GetAxis("Mouse X") * mouseSensitivity, 0)));
-        rb.MovePosition(transform.position + (transform.forward * Input.GetAxis("Vertical") * moveSpeed / 100) + (transform.right * Input.GetAxis("Horizontal") * moveSpeed / 100));
-        if (Input.GetKeyDown("space") && onGround)
-        {
-            rb.AddForce(transform.up * jumpForce * 10, ForceMode.Impulse);
-        }
-    }
+	private void FixedUpdate()
+	{
+		rb.MoveRotation(rb.rotation * Quaternion.Euler(new Vector3(0, Input.GetAxis("Mouse X") * mouseSensitivity, 0)));
+		rb.MovePosition(transform.position + (transform.forward * Input.GetAxis("Vertical") * moveSpeed / 100) + (transform.right * Input.GetAxis("Horizontal") * moveSpeed / 100));
+	}
 
-    private void OnCollisionEnter(Collision collision)
-    {
-        if (collision.gameObject.tag == "Ground")
-        {
-            onGround = true;
-        }
-    }
-    private void OnCollisionExit(Collision collision)
-    {
-        if (collision.gameObject.tag == "Ground")
-        {
-            onGround = false;
-        }
-    }
+	private bool CheckGrounded()
+	{
+		return Physics.Raycast(transform.position, -Vector3.up, 0.5f);	
+	}
 }
